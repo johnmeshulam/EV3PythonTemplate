a8f04db8483039167ffc46d48024eab3109cf5be
simplyCyclops <maipez04@gmail.com>
2020-08-26 09:36:55 +0300

    fixed robot wheel diameter and axel track

--- robot.py
+++ robot.py
@@ -17,7 +17,7 @@ class Robot():
   wheel_left = Motor(Port.B)
   wheel_right = Motor(Port.C)
   chassis = DriveBase(wheel_left, wheel_right,
-                      wheel_diameter=62.4, axle_track=100)
+                      wheel_diameter=54.4, axle_track=108)
 
   arm_left = Motor(Port.A)
   arm_right = Motor(Port.D)
